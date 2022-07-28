cf65edb7716bb102ab2fefdb873355a7cdfc0b49
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-07-28 19:27:29 +0300

    сделано 2 задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        int myInt = 1;
+        short myShort = 2;
+        byte myByte = 120;
+        long myLong = 11232;
+        float myFloat = 111.2f;
+        double myDouble = 14.2;
+        char c = 'b';
+        boolean b = true;
 
+        //Задание 2
+        double boxerWeight = 72.2;
+        double boxer2Weight = 82.7;
+        double BoxersWeight = boxerWeight+boxer2Weight;
+        System.out.println("Общий вес боксеров равен " + BoxersWeight + " кг.");
 
     }
 }
