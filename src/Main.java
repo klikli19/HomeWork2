da9a904ab7ddb80fe7d442a980ed0ce0df7455b1
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-01 19:01:13 +0300

    сделано 2 задания

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int myInt = 1;
         short myShort = 2;
         byte myByte = 120;
-        long myLong = 11232;
+        long myLong = 11232L;
         float myFloat = 111.2f;
         double myDouble = 14.2;
         char c = 'b';
@@ -14,7 +14,9 @@ public class Main {
         double boxerWeight = 72.2;
         double boxer2Weight = 82.7;
         double BoxersWeight = boxerWeight+boxer2Weight;
+        double differenceBoxers = boxer2Weight - boxerWeight;
         System.out.println("Общий вес боксеров равен " + BoxersWeight + " кг.");
+        System.out.println("Разница в весе боксеров составляет " + differenceBoxers + " кг.");
 
     }
 }
